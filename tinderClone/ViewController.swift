9090b47f828957ec25bacc676a2fc93a4b662be4
Kinshuk <kinshukjuneja@gmail.com>
2017-04-06 16:37:44 -0400

    Initial Commit

---
+++ tinderClone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tinderClone
+//
+//  Created by Kinshuk Juneja on 4/6/17.
+//  Copyright © 2017 CS490. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
